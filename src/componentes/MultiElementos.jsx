ccc549e7ccf87687e2db854ed9e165dbda7b21e7
Alisson Dourado <douradonascimento@hotmail.com>
2020-03-21 19:05:28 -0300

    Primeiro commit

---
+++ src/componentes/MultiElementos.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default props => [
+    
+    <h1>Parte 1</h1>,
+    <h2>Parte 2</h2>
+]
+
+// Solução 02
+{/* <React.Fragment>
+    <h1>Parte 1</h1>
+    <h2>Parte 2</h2>
+</React.Fragment> */}
+
+// Solução 01. Mais usada!
+// <div>
+//     <h1>Parte 1</h1>
+//     <h2>Parte 2</h2>
+// </div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom' 
+// import PrimeiroComponente from './componentes/PrimeiroComponente'
+import {CompA, CompB as B} from './componentes/DoisComponentes'
+// import MultiElementos from './componentes/MultiElementos'
+// import FamiliaSilva from './componentes/FamiliaSilva'
+// import Familia from './componentes/Familia'
+// import Membro from './componentes/Membro'
+// import ComponentesComFuncao from './componentes/ComponentesComFuncao';
+// import Pai from './componentes/Pai'
+// import ComponenteClasse from './componentes/ComponenteClasse'
+// import Contador from './componentes/Contador'
+import Hook from './componentes/Hook'
 
+const elemento = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <div>
+        <Hook />
+        {/* <Contador numeroInicial={100} /> */}
+        {/* <ComponenteClasse valor="Sou um componente de Classe!" /> */}
+        {/* <Pai /> */}
+        {/* <ComponentesComFuncao /> */}
+        {/* <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+            <Membro nome="Mariana" />
+        </Familia> */}
+        {/* <FamiliaSilva /> */}
+        {/* <MultiElementos /> */}
+        {/* <CompA valor="Olá eu sou A" /> */}
+        {/* <B valor="B na área!" /> */}
+        {/* <PrimeiroComponente valor="Bom dia!"></PrimeiroComponente> */}
+    </div>    
+, elemento)
